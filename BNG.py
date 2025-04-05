299720660cb73f0aec988f8f8410073fa01c55fc
CYBER-JIHAD <102070525+BNG-71@users.noreply.github.com>
2025-04-05 17:22:24 +0600

    Update BNG.py

--- BNG.py
+++ BNG.py
@@ -1,6 +1,12 @@
 import platform
-b = platform.architecture()[0]
-if b == '64bit':
-    import bngr
-elif b == '32bit':
-    print("32bit Not Supported! Sorry")
+
+class KingProCracker:
+    def __BNG_71__(self):
+        print("Running __BNG_71__ method")
+if __name__ == "__main__":
+    b = platform.architecture()[0]
+    app = KingProCracker()
+    if b == '64bit':
+        app.__BNG_71__()
+    elif b == '32bit':
+        print("32bit Not Supported! Sorry")
