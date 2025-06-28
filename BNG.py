3a9d78ae8c145e2775094441295e10bcb2c9c19e
CYBER-JIHAD <102070525+BNG-71@users.noreply.github.com>
2025-06-28 15:45:07 +0600

    Update BNG.py

--- BNG.py
+++ BNG.py
@@ -1,19 +1,6 @@
-import ctypes
-import os
-
-# .so ফাইলের নাম
-so_file = "khan.cpython-312.so"
-
-# চেক করি ফাইলটা আছে কিনা
-if not os.path.isfile(so_file):
-    print(f"[×] {so_file} ফাইল খুঁজে পাওয়া যায়নি!")
-    exit()
-
-try:
-    # .so ফাইল লোড করা
-    lib = ctypes.CDLL(f"./{so_file}")
-
-    # ধরে নিচ্ছি main() নামে একটা ফাংশন আছে
-    lib.main()
-except Exception as e:
-    print(f"[×] .so ফাইল রান করতে সমস্যা: {e}")
+import platform
+b = platform.architecture()[0]
+if b == '64bit':
+    import OLD
+else:
+    print("32bit Not Supported!")
